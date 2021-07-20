8e010fa9e89cc822fd8af378e6ff56951bfca3fd
Ali Mert Çakar <mail@alimert.net>
2021-07-20 16:06:17 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+function App() {
+  return (
+    <DivContainer>
+      Selam
+    </DivContainer>
+  );
+}
+
+
+const DivContainer = styled.div`
+display:flex; 
+justify-content:center; 
+max-width:1200px;
+margin-top:2rem;
+`;
+export default App;
